f28e0590817fd161e528b73857bcfc6573d07d7e
Manish Mani Dinkar <moneyyiiss@gmail.com>
2023-07-13 19:46:14 +0530

    Assignment solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A{
+
+}
 public class Main {
-  
+  public static void main(String[] args) {
+    B b = new B();
+    String ans = b.meth();
+    System.out.println(ans); 
+  }
 }
